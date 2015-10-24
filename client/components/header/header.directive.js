3f448dd829ea1a32150786199d8bde629f786913
Andrew Kishino <AndrewKishino@gmail.com>
2015-10-24 11:44:08 -0700

    Refactor navigation and header directive

--- client/components/header/header.directive.js
+++ client/components/header/header.directive.js
@@ -27,6 +27,26 @@
     return directive;
 
     function link(scope, elem, attrs) {
+      var geoTable = document.querySelector('.geoTable');
+      var dbUpdate = document.querySelector('.dbUpdate');
+
+      geoTable.addEventListener('click', function(e) {
+        e.preventDefault();
+        var c = dbUpdate.classList.contains('active');
+        if (c) {
+          dbUpdate.classList.remove('active');
+          geoTable.classList.add('active');
+        }
+      });
+
+      dbUpdate.addEventListener('click', function(e) {
+        e.preventDefault();
+        var c = geoTable.classList.contains('active');
+        if (c) {
+          geoTable.classList.remove('active');
+          dbUpdate.classList.add('active');
+        }
+      });
     }
   }
 
